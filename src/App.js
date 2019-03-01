7982e05b33858de0b0ba87bb6a2d5c7953fae5ab
Agustin Mulet <agustin.mulet@gmail.com>
2019-03-01 15:45:54 -0300

    Fix en input de edad y borrado codigo comentado

--- src/App.js
+++ src/App.js
@@ -26,22 +26,6 @@ class App extends Component {
           </Row>
           <PlayersForm />
           <VisiblePlayers />
-          {/*this.state.loaded === null ? (
-          <PlayersTable />
-                <h3>
-                  Please insert data to search, or search with empty inputs to
-                  get the full list of players.
-                </h3>
-              ) : !this.state.loaded ? (
-                <React.Fragment>
-                  <ReactLoading color="#343A40" className="loader" />
-                  <h3>Loading...</h3>
-                </React.Fragment>
-              ) : this.state.players.length > 0 ? (
-                <PlayersTable players={this.state.players} />
-              ) : (
-                <h3>No results found.</h3>
-              )*/}
         </Container>
       </div>
     );
--- src/PlayersForm.js
+++ src/PlayersForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Button, Col, Form, Input, Row } from "reactstrap";
 import { connect } from "react-redux";
-//import getPlayers from "./actionCreators/getPlayers";
 import changeName from "./actionCreators/changeName";
 import changePosition from "./actionCreators/changePosition";
 import changeAge from "./actionCreators/changeAge";
@@ -17,30 +16,23 @@ class PlayersForm extends Component {
                 id="playerName"
                 type="text"
                 defaultValue=""
-                //{this.props.name}
-                //onChange={this.props.handleName}
+                onKeyDown={this.props.handleName}
                 placeholder="Player Name"
               />
             </Col>
             <Col xs="auto" md="3" className="text-center">
-              <Input
-                id="playerPosition"
-                type="select"
-                defaultValue=""
-                //value={this.props.position}
-                //onChange={this.props.handlePosition}
-              >
-                <option value="">Select position</option>
-                <option value="Attacking Midfield">Attacking Midfield</option>
-                <option value="Central Midfield">Central Midfield</option>
-                <option value="Centre-Back">Centre-Back</option>
-                <option value="Centre-Forward">Centre-Forward</option>
-                <option value="Defensive Midfield">Defensive Midfield</option>
-                <option value="Keeper">Keeper</option>
-                <option value="Left Midfield">Left Midfield</option>
-                <option value="Left Wing">Left Wing</option>
-                <option value="Left-Back">Left-Back</option>
-                <option value="Right-Back">Right-Back</option>
+              <Input id="playerPosition" type="select" defaultValue="">
+                <option value=""> Select position </option>
+                <option value="Attacking Midfield"> Attacking Midfield </option>
+                <option value="Central Midfield"> Central Midfield </option>
+                <option value="Centre-Back"> Centre - Back </option>
+                <option value="Centre-Forward"> Centre - Forward </option>
+                <option value="Defensive Midfield"> Defensive Midfield </option>
+                <option value="Keeper"> Keeper </option>
+                <option value="Left Midfield"> Left Midfield </option>
+                <option value="Left Wing"> Left Wing </option>
+                <option value="Left-Back"> Left - Back </option>
+                <option value="Right-Back"> Right - Back </option>
               </Input>
             </Col>
             <Col xs="auto" md="3">
@@ -48,9 +40,10 @@ class PlayersForm extends Component {
                 id="playerAge"
                 type="text"
                 defaultValue=""
-                //value={this.props.age}
-                //onChange={this.props.handleAge}
+                pattern="[0-9]*"
+                onKeyDown={this.props.handleAge}
                 placeholder="Age"
+                maxLength={2}
               />
             </Col>
             <Col xs="auto" md="3" className="text-center">
@@ -73,33 +66,20 @@ const mapStateToProps = ({ name, position, age }) => ({
 
 const mapDispatchToProps = dispatch => ({
   handleName(event) {
-    const re = /^[a-zA-Z_ ]*$/;
-    if (event.target.value === "" || re.test(event.target.value)) {
-      dispatch(changeName(event.target.value));
+    const re = /^[a-zA-Z ]*$/;
+    if (!re.test(event.key)) {
+      event.preventDefault();
     }
   },
-  handlePosition(event) {
-    dispatch(changePosition(event.target.value));
-  },
   handleAge(event) {
-    const re = /^[0-9]{0,2}$/;
-    if (event.target.value === "" || re.test(event.target.value)) {
-      if (
-        event.target.value.length === 2 &&
-        !(
-          parseInt(event.target.value) >= 18 &&
-          parseInt(event.target.value) <= 40
-        )
-      ) {
-        alert("Sorry but the age must be between 18 and 40.");
-      }
-      dispatch(changeAge(event.target.value));
+    const reg = /^[0-9]+$/;
+    if (!(reg.test(event.key) || event.key === "Backspace")) {
+      event.preventDefault();
     }
   },
   handleSubmit(event) {
     event.preventDefault();
     const { playerName, playerPosition, playerAge } = event.target;
-    //console.log(playerName.value, playerPosition.value, playerAge.value);
     if (
       (playerAge.value >= 18 && playerAge.value <= 40) ||
       playerAge.value === ""
@@ -107,10 +87,9 @@ const mapDispatchToProps = dispatch => ({
       dispatch(changeName(playerName.value));
       dispatch(changePosition(playerPosition.value));
       dispatch(changeAge(playerAge.value));
-    }
-    /* else {
+    } else {
       alert("Sorry but the age must be between 18 and 40.");
-    }*/
+    }
   }
 });
 
